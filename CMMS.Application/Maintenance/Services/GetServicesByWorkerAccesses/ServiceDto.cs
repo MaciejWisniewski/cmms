f39923293d1b95848850911054458c5db3f6ecb8
Radosław Wiśniewski <radoslawwisniewski96@gmail.com>
2020-08-19 21:01:50 +0200

    Add TypeName, ScheduledWorkerName and ActualWorkerName to GetServicesByWorkerAccess

--- CMMS.Application/Maintenance/Services/GetServicesByWorkerAccesses/ServiceDto.cs
+++ CMMS.Application/Maintenance/Services/GetServicesByWorkerAccesses/ServiceDto.cs
@@ -8,8 +8,11 @@ namespace CMMS.Application.Maintenance.Services.GetServicesByWorkerAccesses
         public Guid ResouceId { get; set; }
         public string ResourceName { get; set; }
         public Guid TypeId { get; set; }
+        public string TypeName { get; set; }
         public Guid ScheduledWorkerId { get; set; }
+        public string ScheduledWorkerName { get; set; }
         public Guid? ActualWorkerId { get; set; }
+        public string ActualWorkerName { get; set; }
         public DateTime ScheduledStartDateTime { get; set; }
         public DateTime ScheduledEndDateTime { get; set; }
         public DateTime? ActualStartDateTime { get; set; }
